5ee8446f0df9da43e458edad1a987065c5b3d9bf
altmsahjlyl91 <166942121+altmsahjlyl91@users.noreply.github.com>
2024-04-15 00:24:14 +0300

    Update mp3-songs.js

--- plugins/mp3-songs.js
+++ plugins/mp3-songs.js
@@ -1,113 +1,89 @@
-
-import fetch from 'node-fetch';
-import axios from 'axios';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
+import fetch from  node-fetch ;
+import axios from  axios ;
+import {youtubedl, youtubedlv2} from  @bochilteam/scraper ;
 import fs from "fs";
-import yts from 'yt-search';
+import yts from  yt-search ;
 
 let limit1 = 100;
 let limit2 = 400;
 let limit_a1 = 50;
 let limit_a2 = 400;
+const handler = async (m, {conn, command, args, text, usedPrefix}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.descargas_play
 
-const handler = async (m, { conn, command, args, text, usedPrefix }) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] قم بإدخال اسم الأغنية المطلوبة للبحث عنها.*\n\n*—◉ المثال:*\n*${usedPrefix + command} ثم اكتب اسم ماتريد تحميلة*`;
-
-  const yt_play = await search(args.join(' '));
-  let additionalText = '';
-
-  if (command === 'play','1صوت') {
-    additionalText = 'صوت 🔊';
-  } else if (command === 'play2', '1فيديو') {
-    additionalText = 'فيديو 🎥';
-  }
-
-  const texto1 = `*◉——⌈🔊 تشغيل يوتيوب 🔊⌋——◉*\n
-❏ 📌 *العنوان:* ${yt_play[0].title}
-❏ 📆 *تم النشر:* ${yt_play[0].ago}
-❏ ⌚ *المدة:* ${secondString(yt_play[0].duration.seconds)}
-❏ 👀 *المشاهدات:* ${`${MilesNumber(yt_play[0].views)}`}
-❏ 👤 *المؤلف:* ${yt_play[0].author.name}
-❏ ⏯️ *القناة:* ${yt_play[0].author.url}
-❏ 🆔 *المعرف:* ${yt_play[0].videoId}
-❏ 🪬 *النوع:* ${yt_play[0].type}
-❏ 🔗 *الرابط:* ${yt_play[0].url}\n
-❏ *_جاري إرسال ${additionalText}، يرجى الانتظار..._*`.trim();
-
-  conn.sendMessage(m.chat, { image: { url: yt_play[0].thumbnail }, caption: texto1 }, { quoted: m });
-
-  if (command == 'play') {
-    try {
-      const audio = global.API('ApiEmpire', `/api/v1/ytmp3?url=${yt_play[0].url}`);
-      const ttl = await yt_play[0].title;
-      const buff_aud = await getBuffer(audio);
-      const fileSizeInBytes = buff_aud.byteLength;
-      const fileSizeInKB = fileSizeInBytes / 1024;
-      const fileSizeInMB = fileSizeInKB / 1024;
-      const size = fileSizeInMB.toFixed(2);
-
-      if (size >= limit_a2) {
-        await conn.sendMessage(m.chat, { text: `*[ ✔ ] قم بتنزيل الفيديو من هنا ${audio}*` }, { quoted: m });
-        return;
-      }
-
-      if (size >= limit_a1 && size <= limit_a2) {
-        await conn.sendMessage(m.chat, { document: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3` }, { quoted: m });
-        return;
-      } else {
-        await conn.sendMessage(m.chat, { audio: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3` }, { quoted: m });
-        return;
-      }
-    } catch {
-      throw '*[❗] حدث خطأ، يرجى المحاولة مرة أخرى.*';
+  if (!text) throw `${tradutor.texto1[0]} _${usedPrefix + command} ${tradutor.texto1[1]}`;
+    const yt_play = await search(args.join(   ));
+    let additionalText =   ;
+    if (command ===  play ) {
+      additionalText =  audio ;
+    } else if (command ===  play2 ) {
+      additionalText =  vídeo ;
     }
-  }
-
-  if (command == 'play2') {
-    try {
-      const video = global.API('ApiEmpire', `/api/v1/ytmp4?url=${yt_play[0].url}`);
-      const ttl2 = await yt_play[0].title;
-      const buff_vid = await getBuffer(video);
-      const fileSizeInBytes2 = buff_vid.byteLength;
-      const fileSizeInKB2 = fileSizeInBytes2 / 1024;
-      const fileSizeInMB2 = fileSizeInKB2 / 1024;
-      const size2 = fileSizeInMB2.toFixed(2);
-
-      if (size2 >= limit2) {
-        await conn.sendMessage(m.chat, { text: `*[ ✔ ] قم بتنزيل الفيديو من هنا ${video}*` }, { quoted: m });
-        return;
-      }
-
-      if (size2 >= limit1 && size2 <= limit2) {
-        await conn.sendMessage(m.chat, { document: buff_vid, mimetype: 'video/mp4', fileName: ttl2 + `.mp4` }, { quoted: m });
-        return;
-      } else {
-        await conn.sendMessage(m.chat, { video: buff_vid, mimetype: 'video/mp4', fileName: ttl2 + `.mp4` }, { quoted: m });
-        return;
-      }
-    } catch {
-      throw '*[❗] حدث خطأ، يرجى المحاولة مرة أخرى.*';
+    const texto1 = `${tradutor.texto2[0]} ${yt_play[0].title}\n\n${tradutor.texto2[1]} ${yt_play[0].ago}\n\n${tradutor.texto2[2]} ${secondString(yt_play[0].duration.seconds)}\n\n${tradutor.texto2[3]} ${`${MilesNumber(yt_play[0].views)}`}\n\n${tradutor.texto2[4]} ${yt_play[0].author.name}\n\n${tradutor.texto2[5]} ${yt_play[0].videoId}\n\n${tradutor.texto2[6]} ${yt_play[0].type}\n\n${tradutor.texto2[7]} ${yt_play[0].url}\n\n${tradutor.texto2[8]} ${yt_play[0].author.url}\n\n${tradutor.texto2[9]} ${additionalText}. ${tradutor.texto2[10]}`.trim();
+    conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
+    if (command ==  play ) {
+    try {   
+    const audio = global.API( CFROSAPI , `/api/v1/ytmp3?url=${yt_play[0].url}`)
+    const ttl = await yt_play[0].title
+    const buff_aud = await getBuffer(audio);
+    const fileSizeInBytes = buff_aud.byteLength;
+    const fileSizeInKB = fileSizeInBytes / 1024;
+    const fileSizeInMB = fileSizeInKB / 1024;
+    const size = fileSizeInMB.toFixed(2);       
+    if (size >= limit_a2) {  
+    await conn.sendMessage(m.chat, {text: `${tradutor.texto3} _${audio}_`}, {quoted: m});
+    return;    
+    }     
+    if (size >= limit_a1 && size <= limit_a2) {  
+    await conn.sendMessage(m.chat, {document: buff_aud, mimetype:  audio/mpeg , fileName: ttl + `.mp3`}, {quoted: m});   
+    return;
+    } else {
+    await conn.sendMessage(m.chat, {audio: buff_aud, mimetype:  audio/mpeg , fileName: ttl + `.mp3`}, {quoted: m});   
+    return;    
+    }} catch {
+    throw tradutor.texto4;    
+    }}
+    if (command ==  play2 ) {
+    try {   
+    const video = global.API( CFROSAPI , `/api/v1/ytmp4?url=${yt_play[0].url}`)
+    const ttl2 = await yt_play[0].title
+    const buff_vid = await getBuffer(video);
+    const fileSizeInBytes2 = buff_vid.byteLength;
+    const fileSizeInKB2 = fileSizeInBytes2 / 1024;
+    const fileSizeInMB2 = fileSizeInKB2 / 1024;
+    const size2 = fileSizeInMB2.toFixed(2);       
+    if (size2 >= limit2) {  
+    await conn.sendMessage(m.chat, {text: `${tradutor.texto5} _${video}_`}, {quoted: m});
+    return;    
+    }     
+    if (size2 >= limit1 && size2 <= limit2) {  
+    await conn.sendMessage(m.chat, {document: buff_vid, mimetype:  video/mp4 , fileName: ttl2 + `.mp4`}, {quoted: m});   
+    return;
+    } else {
+    await conn.sendMessage(m.chat, {video: buff_vid, mimetype:  video/mp4 , fileName: ttl2 + `.mp4`}, {quoted: m});   
+    return;    
+    }} catch {
+    throw tradutor.texto6;    
     }
   }
 };
-
-handler.help = ['play', 'play2'].map((v) => v + ' <اسم الأغنية>');
-handler.tags = ['downloader'];
-handler.command = /^(play|play2|صوت1|فيديو1)$/i;
-
+handler.command = /^(play|play2)$/i;
 export default handler;
 
 async function search(query, options = {}) {
-  const search = await yts.search({ query, hl: 'ar', gl: 'EG', ...options });
+  const search = await yts.search({query, hl:  es , gl:  ES , ...options});
   return search.videos;
 }
 
 function MilesNumber(number) {
   const exp = /(\d)(?=(\d{3})+(?!\d))/g;
-  const rep = '$1.';
-  const arr = number.toString().split('.');
+  const rep =  $1. ;
+  const arr = number.toString().split( . );
   arr[0] = arr[0].replace(exp, rep);
-  return arr[1] ? arr.join('.') : arr[0];
+  return arr[1] ? arr.join( . ) : arr[0];
 }
 
 function secondString(seconds) {
@@ -116,17 +92,17 @@ function secondString(seconds) {
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
-  const dDisplay = d > 0 ? d + (d == 1 ? ' يوم، ' : ' أيام، ') : '';
-  const hDisplay = h > 0 ? h + (h == 1 ? ' ساعة، ' : ' ساعات، ') : '';
-  const mDisplay = m > 0 ? m + (m == 1 ? ' دقيقة، ' : ' دقائق، ') : '';
-  const sDisplay = s > 0 ? s + (s == 1 ? ' ثانية' : ' ثواني') : '';
+  const dDisplay = d > 0 ? d + (d == 1 ?  d   :  d  ) :   ;
+  const hDisplay = h > 0 ? h + (h == 1 ?  h   :  h  ) :   ;
+  const mDisplay = m > 0 ? m + (m == 1 ?  m   :  m  ) :   ;
+  const sDisplay = s > 0 ? s + (s == 1 ?  s  :  s ) :   ;
   return dDisplay + hDisplay + mDisplay + sDisplay;
 }
 
 function bytesToSize(bytes) {
   return new Promise((resolve, reject) => {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return 'n/a';
+    const sizes = [ Bytes ,  KB ,  MB ,  GB ,  TB ];
+    if (bytes === 0) return  n/a ;
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
     if (i === 0) resolve(`${bytes} ${sizes[i]}`);
     resolve(`${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`);
@@ -134,13 +110,7 @@ function bytesToSize(bytes) {
 }
 
 const getBuffer = async (url, options) => {
-  options ? options : {};
-  const res = await axios({
-    method: 'get',
-    url,
-    headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 },
-    ...options,
-    responseType: 'arraybuffer'
-  });
-  return res.data;
+    options ? options : {};
+    const res = await axios({method:  get , url, headers: { DNT : 1,  Upgrade-Insecure-Request : 1,}, ...options, responseType:  arraybuffer });
+    return res.data;
 };
