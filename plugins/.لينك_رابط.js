c2037c20ff4c7d9bf967d88e15138d7cd1af89d7
altmsahjlyl91 <166942121+altmsahjlyl91@users.noreply.github.com>
2024-04-14 03:24:53 +0300

    Add files via upload

---
+++ plugins/.لينك_رابط.js
@@ -0,0 +1,20 @@
+//prince lo9mane
+import { areJidsSameUser } from '@adiwajshing/baileys'
+let handler = async (m, { conn, args }) => {
+    let group = m.chat
+    if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'لايمكننك استخدام هاذا الامر الى في المجموعات'
+    let groupMetadata = await conn.groupMetadata(group)
+    if (!groupMetadata) throw 'groupMetadata is undefined :\\'
+    if (!('participants' in groupMetadata)) throw 'participants is not defined :('
+    let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
+    if (!me) throw 'I\'m not in that group :('
+    if (!me.admin) throw 'I\'يجب ان يكون البوت مشرف اولا...!'
+    m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
+}
+handler.help = ['linkgroup']
+handler.tags = ['group']
+handler.command = /^link|رابط|الرابط(gro?up)?$/i
+
+
+export default handler
