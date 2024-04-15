5bbf422299038b601d32c6ee9685712e50146322
altmsahjlyl91 <166942121+altmsahjlyl91@users.noreply.github.com>
2024-04-16 02:56:03 +0300

    Update .لينك_رابط.js

--- plugins/.لينك_رابط.js
+++ plugins/.لينك_رابط.js
@@ -4,7 +4,7 @@ let link = 'لينك الجروب✨💜: https://chat.whatsapp.com/' + await co
 conn.reply(m.chat, link, m, {detectLink: true})
 //conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
 }
-handler.command = /^لينك?$/i
+handler.command = /^رابط|لينك?$/i
 handler.group = true
 handler.botAdmin = true
 export default handler
